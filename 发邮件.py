4c3e2b3204b8edbf38a331ec80f66a84652b50fd
wyfmdy <92067816+wyfmdy@users.noreply.github.com>
2021-11-10 10:30:49 +0800

    Add files via upload

--- 发邮件.py
+++ 发邮件.py
@@ -1,19 +1,64 @@
+"""
+    1.加载所有的测试用例
+    2.执行用例并生成测试报告
+"""
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 from HTMLTestRunner import HTMLTestRunner
 import unittest
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from BankTest import  da_addUser
 
-#1.将所有用例全部加载出来
-tests = unittest.defaultTestLoader.discover(r"E:\PythonTest",pattern="*test.py")
+# 1.加载所有用例
+tests = unittest.defaultTestLoader.discover(r"D:\pythonProject2day1111\BankTest.py", pattern="Test*.py",top_level_dir=None)
 
 
-#2.创建运行器
+# 2.创建运行器
 runner = HTMLTestRunner.HTMLTestRunner(
-    title = "这是计算机测试报告",
-    description = "这是计算机的加减法测试报告",
+    title="计算器的测试报告",
+    description="这是综合测试报告",
     verbosity=1,
-    stream= open(file="计算机报告.html",mode="w+",encoding="utf-8")
+    stream=open(file="calculator.html", mode="w+", encoding="utf-8")
 )
 
-
-
-#3.让运行器运行用例，并生成测试报告
+# 3.执行用例
 runner.run(tests)
+
+
+# 4.任务： 使用邮件发送附件，把报告发送给我
+def send_email(name):
+    sender = '152666564@qq.com'
+    passwd = "hjashdjkwafas"
+    receivers = '454614867@qq.com'
+    subject = '计算器综合测试'
+
+    # 构造邮件对象
+    message = MIMEMultipart()
+    message['From'] = Header("shinensama", 'utf-8')
+    message['To'] = Header("Jason", 'utf-8')
+    message['Subject'] = Header(subject, 'utf-8')
+    message_content = "附件为测试结果"
+    message.attach(MIMEText(message_content, 'plain', 'utf-8'))
+
+    # 添加附件
+    with open(name, mode='rb') as f:
+        attfile = f.read()
+    att1 = MIMEApplication(attfile)
+    att1["Content-Type"] = 'application/octet-stream'
+    att1["Content-Disposition"] = 'attachment; filename="%s"' % name
+    message.attach(att1)
+
+    # 发送邮件
+    try:
+        smtpObj = smtplib.SMTP_SSL("smtp.qq.com", 465)
+        smtpObj.login(sender, passwd)
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        smtpObj.quit()
+        print("邮件发送成功")
+    except smtplib.SMTPException as cause:
+        print("无法发送邮件", cause)
+
+
+send_email("calculator.html")
